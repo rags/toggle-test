b1b30000ce25b503f3b67f32d40e5b96bcac09b4
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2013-03-07 03:32:15 +0530

    extension complete. Have to polish it and add UI tests

--- toggle-test.el
+++ toggle-test.el
@@ -9,22 +9,31 @@
 ;; 2. Source folder(s) (relative to root directory)
 ;; 3. Test source folder(s) (relative to root directory)
 ;; 4. prefix/suffix attached to source file names to name the test files
-;;    Ex: src/foo.py test/test_foo.py - the prefix is 'test_'
-;;        src/controllers/Foo.scala specs/controllers/Foo$Spec.scala - the suffix here is '$Spec'
-;;    Note: you can specify both prefix and suffix if required 
+;;    Ex: src/foo.py <-> test/test_foo.py - the prefix is 'test_'
+;;        src/controllers/Foo.scala <-> specs/controllers/Foo$Spec.scala 
+;;        The suffix here is '$Spec'.
+;;    Note: you can specify both prefix and suffix if required.
 ;;
 ;; Usage:
-;; (add-to-list tgt-projects '((:root-dir "~/python-project") (:src-dirs "src") (:test-dirs "tests") (:test-prefixes "test-")))          
-;; (add-to-list tgt-projects '((:root-dir "~/scala-project") (:src-dirs "src") (:test-dirs "specs") (:test-suffixes "$Spec")))
-(defcustom tgt-projects '() "Project entries. 1 entry per project that provides naming convention and folder structure"
-:group 'toggle-test
-:type '(repeat (alist)))
+;; (add-to-list tgt-projects '((:root-dir "~/python-project") 
+;;                             (:src-dirs "src") (:test-dirs "tests") 
+;;                             (:test-prefixes "test-")))          
+;; (add-to-list tgt-projects '((:root-dir "~/scala-project") 
+;;                             (:src-dirs "src") (:test-dirs "specs") 
+;;                             (:test-suffixes "$Spec")))
+(defcustom tgt-projects '() 
+  "Project entries. 
+One entry per project that provides naming convention and folder structure"
+  :group 'toggle-test
+  :type '(repeat (alist)))
 
-;; Indicates if the toggle file should be opened in a new window or replace the buffer in current window. The default behavior is to open it in new window
+;; Indicates if the toggle file should be opened in a new window 
+;; or replace the buffer in current window. The default behavior is to open it in new window
 ;; Use (setq tgt-open-in-new-window 'nil) to override default behavior
-(defcustom tgt-open-in-new-window t "Indicates if the files are opened in new window or current window"
-:group 'toggle-test
-:type 'boolean)
+(defcustom tgt-open-in-new-window t 
+  "Indicates if the files are opened in new window or current window"
+  :group 'toggle-test
+  :type 'boolean)
 
 (defun tgt-proj-prop (prop proj) (cdr (assoc prop proj)))
 (defun tgt-root-dir (proj) (car (tgt-proj-prop :root-dir proj)))
@@ -35,15 +44,16 @@
      (or cur_val 
 	 (let ((src-dir (file-name-as-directory 
 			 (expand-file-name dir (tgt-root-dir proj))))) 
-	   (if (tgt-is-ancestor? src-dir file)
+	   (if (tgt-is-ancestor-p src-dir file)
 	       (subseq file (length src-dir)))))) 
    (cdr (assoc dir-type proj))
    :initial-value 'nil))
 
-; Given a file return its project (returns first match. Doesnt handle sub directories added as different project)
+;; Given a file return its project 
+;; (returns first match. Doesnt handle sub directories added as different project)
 (defun tgt-proj-for (file)
   (car (remove-if-not
-		(lambda (proj) (tgt-is-ancestor? (tgt-root-dir proj) file)) tgt-projects)))
+		(lambda (proj) (tgt-is-ancestor-p (tgt-root-dir proj) file)) tgt-projects)))
 
 (defun tgt-find-project-file-in-dirs (file proj)
   (let ((src-file-rel-path (tgt-relative-file-path file proj :src-dirs)))
@@ -58,27 +68,33 @@
 	       (src-file-rel-path test-file-rel-path) 
 	       (tgt-find-project-file-in-dirs file proj)
 	     (cond
-	      (src-file-rel-path (tgt-all-toggle-paths src-file-rel-path proj :test-dirs))
-	      (test-file-rel-path (tgt-all-toggle-paths test-file-rel-path proj :src-dirs))
+	      (src-file-rel-path (tgt-all-toggle-paths 
+							  src-file-rel-path proj :test-dirs 
+							  #'tgt-possible-test-file-names))
+	      (test-file-rel-path (tgt-all-toggle-paths 
+							   test-file-rel-path proj :src-dirs 
+							   #'tgt-possible-src-file-names))
 	      (t (message "File '%s' in project '%s' is not part src-dirs or test-dirs"
 			  file (tgt-root-dir proj)) 'nil))))
 	  (t (message "File '%s' not part of any project. Have you defined a project?" file) 
 		 'nil))))
 
-(defun tgt-best-matches (all-matches))
+(defun tgt-best-matches (all-matches)
+  (let ((exact-matches (remove-if-not #'file-exists-p all-matches)))
+	(if exact-matches (values t exact-matches) (values 'nil all-matches))))
 
-(defun tgt-all-toggle-paths (rel-path proj dir-type)
+(defun tgt-all-toggle-paths (rel-path proj dir-type file-names-generator)
   (tgt-make-full-paths 
    (tgt-possible-dirs proj dir-type (file-name-directory rel-path)) 
-   (tgt-possible-file-names 
-	(file-name-nondirectory rel-path) 
-	(tgt-proj-prop :test-prefixes proj) 
-	(tgt-proj-prop :test-suffixes proj))))
+   (funcall file-names-generator 
+			(file-name-nondirectory rel-path) 
+			(tgt-proj-prop :test-prefixes proj) 
+			(tgt-proj-prop :test-suffixes proj))))
 
-(defun tgt-full-make-paths (dirs filenames)
+(defun tgt-make-full-paths (dirs filenames)
   (tgt-cross-join dirs filenames (lambda (dir file) (expand-file-name file dir))))
 
-;rel-dir-path is com/foo/bar for src in "....src/com/foo/bar/Blah.java
+;rel-dir-path is com/foo/bar for src in "proj-root/src/com/foo/bar/Blah.java
 (defun tgt-possible-dirs (proj dir-type rel-dir-path)
   (let ((root (tgt-root-dir proj)))
 	(mapcar (lambda (dir) (expand-file-name rel-dir-path (expand-file-name dir root))) 
@@ -140,45 +156,46 @@
 				(add-to-list 'ret-val (if fn (funcall fn i j) (list i j)))))
 			 ret-val))))
 
-(defun tgt-open (file) (funcall (if tgt-open-in-new-window #'find-file 
-				  #'find-file-other-window) file))
+(defun tgt-open-file (file)
+  (tgt-find-file file (if tgt-open-in-new-window #'find-file-other-window #'find-file)))
+
+(defun tgt-find-file (file find-file-fn)
+  (mkdir (file-name-directory file) t);Ensure parent directory exits
+  (funcall find-file-fn file))
+
+
+(defun tgt-show-matches (matches exact-match-p)
+  (with-output-to-temp-buffer "*Toggle Test*"  
+	(funcall (if tgt-open-in-new-window 
+				 #'switch-to-buffer-other-window #'switch-to-buffer) "*Toggle Test*")
+	(princ (if exact-match-p 
+			   "Mutiple matching files were found. Choose one to open:\n"
+			 "No matching file found. These are the potentials. Pick one to create:\n"))
+	(dolist (file matches)
+	  (princ "* ")
+	  (insert-button file 'action (lambda (btn) 
+									(tgt-find-file 
+									 (button-label btn) #'find-alternate-file)))
+	  (princ "\n"))))
+
+
+(defun tgt-open (files)
+  (if files
+	  (multiple-value-bind (exact-match-p matches) (tgt-best-matches files)
+		(cond
+		 ((= 1 (length matches)) (tgt-open-file (car matches)))
+		 (t (tgt-show-matches matches exact-match-p))))))
 
 (defun tgt-toggle ()
-  (let ((toggle-file (tgt-find-match (file-truename buffer-file-name)))) 
-    (if toggle-file 
-	(tgt-open toggle-file))))
+  (tgt-open (tgt-find-match (file-truename buffer-file-name))))
 
-(defun tgt-is-ancestor? (dir file)
+(defun tgt-is-ancestor-p (dir file)
   (if (and dir file (> (length file) 0) (> (length dir) 0))
    (let ((dir-name (file-name-as-directory (expand-file-name dir))) 
 	 (file-name (expand-file-name file)))
 	 (and (>= (length file-name) (length dir-name)) 
 	      (string= (substring file-name 0 (length dir-name)) dir-name))) 'nil))
 
-
-;;;;;;;;;;;;;;;;;;;;CRAP
-(defun toggle-test ()
-  (interactive)
-  (let* ((proj-root (file-truename (ffip-project-root)))
-	 (f (file-truename buffer-file-name))
-	 (fname (file-name-nondirectory f)))
-    (find-file-other-window
-     (if (string-match "/tests/" f)
-	 (replace-regexp-in-string "test_" "" 
-				   (replace-regexp-in-string "tests/" "src/" f))
-       (replace-regexp-in-string fname (concat "test_" fname) (replace-regexp-in-string (concat proj-root "src/") (concat proj-root "tests/") f))))))
-
-(setq x `((:a  1) (:b-c  "2")))
-(string= (cadr (assoc :b-c '())) "2")
-(add-to-list 'flymake-master-file-dirs "./tests")
-
-'(with-output-to-temp-buffer "xxx"  (switch-to-buffer "xxx")
-
-(princ "Choose a file:\n")
-(insert-button "file1" 'action (lambda (x) (find-file "/tmp/file1")))
-(princ "\n")
-(insert-button "file2" 'action (lambda (x) (find-alternate-file "/tmp/file2"))))
-(expand-file-name (file-name-as-directory "foo/bar//../blah") "/tmp") 
 
 ;;;;;;;;;;;;;;;;;;;;;;; TESTS ;;;;;;;;;;;;;;;;;;;;;
 
@@ -192,7 +209,7 @@
 			   '((:root-dir "/tmp/projects/scala-proj") 
 				 (:src-dirs "play/app" "foo-module/src") 
 				 (:test-dirs "integration-specs" "play/specs" "foo-module/tests") 
-				 (:test-suffixes "$Spec.scala" "Spec.scala" "Test.scala" "$Test.scala")))
+				 (:test-suffixes "$Spec" "Spec" "Test" "$Test")))
   tgt-projects)
 
 
@@ -231,13 +248,13 @@
 		      py-proj :test-dirs)))))
 
 (ert-deftest should-indentify-ancestor ()
-(should (tgt-is-ancestor? "/foo" "/foo/bar"))
-(should-not (tgt-is-ancestor? "/foo/b" "/foo/bar"))
-(should-not (tgt-is-ancestor? "" "foobar"))
-(should-not (tgt-is-ancestor? 'nil "foobar"))
-(should-not  (tgt-is-ancestor? 'nil 'nil))
-(should-not (tgt-is-ancestor? "hello" 'nil))
-(should-not  (tgt-is-ancestor? "foobar" "f0")))
+(should (tgt-is-ancestor-p "/foo" "/foo/bar"))
+(should-not (tgt-is-ancestor-p "/foo/b" "/foo/bar"))
+(should-not (tgt-is-ancestor-p "" "foobar"))
+(should-not (tgt-is-ancestor-p 'nil "foobar"))
+(should-not  (tgt-is-ancestor-p 'nil 'nil))
+(should-not (tgt-is-ancestor-p "hello" 'nil))
+(should-not  (tgt-is-ancestor-p "foobar" "f0")))
 
 (ert-deftest should-identify-the-project-given-a-file ()
   (setq tgt-projects '())
@@ -275,10 +292,28 @@
   (should (equal '("foo.rb")  
 				 (tgt-possible-src-file-names "foo_spec.rb" '() '("_test" "_spec"))))
   (should (equal '("foo.rb") (tgt-possible-src-file-names "foo.rb" '() '())))
-  (should (equal '("Blah.scala" "BlahSpec.scala" "UnitBlah.scala") (tgt-possible-src-file-names "UnitBlahSpec.scala" '("Unit" "Integration" "Functional") '("Spec" "$Spec"))))
-  (should (equal '("Blah.scala") (tgt-possible-src-file-names "FunctionalBlah.scala" '("Unit" "Integration" "Functional") '("Spec" "$Spec"))))
-  (should (equal '("Blah$.scala" "Blah.scala") (tgt-possible-src-file-names "Blah$Spec.scala" '("Unit" "Integration" "Functional") '("Spec" "$Spec"))))
-  (should (equal '("Blah.scala" "Blah$.scala" "Blah$Spec.scala" "FunctionalBlah$.scala" "FunctionalBlah.scala") (tgt-possible-src-file-names "FunctionalBlah$Spec.scala" '("Unit" "Integration" "Functional") '("Spec" "$Spec")))))
+  (should (equal '("Blah.scala" "BlahSpec.scala" "UnitBlah.scala") 
+				 (tgt-possible-src-file-names "UnitBlahSpec.scala" 
+											  '("Unit" "Integration" "Functional") 
+											  '("Spec" "$Spec"))))
+  (should (equal '("Blah.scala") 
+				 (tgt-possible-src-file-names "FunctionalBlah.scala" 
+											  '("Unit" "Integration" "Functional") 
+											  '("Spec" "$Spec"))))
+  (should (equal '("Blah$.scala" "Blah.scala") 
+				 (tgt-possible-src-file-names "Blah$Spec.scala" 
+											  '("Unit" "Integration" "Functional") 
+											  '("Spec" "$Spec"))))
+  (should (equal '("Blah.scala" "Blah$.scala" 
+				   "Blah$Spec.scala" "FunctionalBlah$.scala" 
+				   "FunctionalBlah.scala") 
+				 (tgt-possible-src-file-names "FunctionalBlah$Spec.scala" 
+											  '("Unit" "Integration" "Functional") 
+											  '("Spec" "$Spec"))))
+  (should (equal '("Blah.scala") (tgt-possible-src-file-names 
+								 "BlahTest.scala" 
+								 'nil 
+								 '("Spec" "$Spec" "Test" "$Test")))))
 
 (ert-deftest should-generate-possible-test-filename ()
   (should (equal '("test_foo.py" "spec_foo.py")  
@@ -286,8 +321,56 @@
   (should (equal '("foo_test.rb" "foo_spec.rb")  
 				 (tgt-possible-test-file-names "foo.rb" '() '("_test" "_spec"))))
   (should (equal '("foo.rb") (tgt-possible-test-file-names "foo.rb" '() '())))
-  (should (equal '("FunctionalBlah$Spec.scala" "FunctionalBlahSpec.scala" "IntegrationBlah$Spec.scala" "IntegrationBlahSpec.scala" "UnitBlah$Spec.scala" "UnitBlahSpec.scala" "UnitBlah.scala" "IntegrationBlah.scala" "FunctionalBlah.scala" "BlahSpec.scala" "Blah$Spec.scala") (tgt-possible-test-file-names "Blah.scala" '("Unit" "Integration" "Functional") '("Spec" "$Spec")))))
+  (should (equal '("FunctionalBlah$Spec.scala" "FunctionalBlahSpec.scala" 
+				   "IntegrationBlah$Spec.scala" "IntegrationBlahSpec.scala"
+				   "UnitBlah$Spec.scala" "UnitBlahSpec.scala" 
+				   "UnitBlah.scala" "IntegrationBlah.scala" 
+				   "FunctionalBlah.scala" "BlahSpec.scala" "Blah$Spec.scala") 
+				 (tgt-possible-test-file-names "Blah.scala" 
+											   '("Unit" "Integration" "Functional") 
+											   '("Spec" "$Spec")))))
 
 (ert-deftest should-generate-all-possible-toggle-paths ()
   (multiple-value-bind (scala-proj py-proj) (setup-test-projects) 
-	(tgt-all-toggle-paths "foo/bar/Blah.scala" scala-proj :test-dirs)))
+	(should (equal '("/tmp/projects/python-proj/tests/foo/test___init__.py")
+				   (tgt-all-toggle-paths "foo/__init__.py" py-proj 
+										 :test-dirs #'tgt-possible-test-file-names)))
+	(should (equal '("/tmp/projects/python-proj/src/foo/blah.py")
+				   (tgt-all-toggle-paths "foo/test_blah.py" py-proj 
+										 :src-dirs #'tgt-possible-src-file-names)))
+	(should (equal 
+			 '("/tmp/projects/scala-proj/foo-module/tests/foo/bar/Blah$Test.scala" 
+			   "/tmp/projects/scala-proj/foo-module/tests/foo/bar/BlahTest.scala"
+			   "/tmp/projects/scala-proj/foo-module/tests/foo/bar/BlahSpec.scala" 
+			   "/tmp/projects/scala-proj/foo-module/tests/foo/bar/Blah$Spec.scala"
+			   "/tmp/projects/scala-proj/play/specs/foo/bar/Blah$Test.scala" 
+			   "/tmp/projects/scala-proj/play/specs/foo/bar/BlahTest.scala"
+			   "/tmp/projects/scala-proj/play/specs/foo/bar/BlahSpec.scala" 
+			   "/tmp/projects/scala-proj/play/specs/foo/bar/Blah$Spec.scala"
+			   "/tmp/projects/scala-proj/integration-specs/foo/bar/Blah$Test.scala"
+			   "/tmp/projects/scala-proj/integration-specs/foo/bar/BlahTest.scala"
+			   "/tmp/projects/scala-proj/integration-specs/foo/bar/BlahSpec.scala"
+			   "/tmp/projects/scala-proj/integration-specs/foo/bar/Blah$Spec.scala")
+			 (tgt-all-toggle-paths "foo/bar/Blah.scala" scala-proj 
+								   :test-dirs #'tgt-possible-test-file-names)))
+	(should (equal 
+			 '("/tmp/projects/scala-proj/foo-module/src/controllers/Blah.scala" 
+			   "/tmp/projects/scala-proj/play/app/controllers/Blah.scala")  
+			 (tgt-all-toggle-paths "controllers/BlahTest.scala" scala-proj 
+							   :src-dirs #'tgt-possible-src-file-names)))))
+
+(ert-deftest should-filter-existing-files ()  
+  (should (equal `(,'nil ,'nil) (tgt-best-matches 'nil)))
+  (let ((file1 (make-temp-file "foo")) 
+		(file2 (make-temp-file "bar")))
+	(should (equal `(,'nil ("/tmp/foo/1" "/tmp/foo/3" "/tmp/foo/2")) 
+		 (tgt-best-matches '("/tmp/foo/1" "/tmp/foo/3" "/tmp/foo/2"))))
+	(should (equal 
+			 `(,t (,file1)) 
+			 (tgt-best-matches (list file1 "/tmp/f00" "/tmp/b00"))))
+	(should (equal 
+			 `(,t (,file2 ,file1)) 
+			 (tgt-best-matches (list file2 "/doesnt-exist/1" file1))))))
+
+
+
